18897417bba6f3b5a6e228814ba8171ee59906f8
Mimo <mimoriam420@gmail.com>
2022-11-30 19:47:06 +0500

    Commit #3 - Preliminary work. Proper project work after this commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import {
+    BrowserRouter as Router,
+} from 'react-router-dom';
+
+import { Navbar } from "./components/layout/Navbar";
+
 export const App = () => {
     return (
-        <>
-            aaa
-        </>
+        <Router>
+            <div className="flex flex-col h-screen justify-between">
+                <Navbar/>
+            </div>
+
+            <div>
+                Content
+            </div>
+        </Router>
     )
 }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,11 @@
+import { FaGithub } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+
 export const Navbar = () => {
     return (
         <>
-
+            Navbar
         </>
     )
 }
